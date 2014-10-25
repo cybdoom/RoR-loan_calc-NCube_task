135c28af18df76e80726a93b5b5eed5d95a3f4f9
Eugene Narovlyansky <cybdoom@gmail.com>
2014-10-25 15:54:37 +0300

    Added lobby page markup Implemented models and helpers mechanism

--- server.rb
+++ server.rb
@@ -1,2 +1,23 @@
 require 'sinatra'
-require './routes.rb'
+require './routes'
+
+class Server
+  ROOT = File.dirname(__FILE__)
+
+  def initialize
+    load_models
+    load_helpers
+  end
+
+  private
+
+  def load_models
+    Dir[File.join ROOT, 'models', '**', '*'].each { |filename| require filename }
+  end
+
+  def load_helpers
+    Dir[File.join ROOT, 'helpers', '**', '*'].each { |filename| require filename }
+  end
+end
+
+Server.new
